bfcce8b7d3db279d6b99d7b07238c072b3f62ddd
user <brad@brad.tw>
2019-04-27 15:54:17 +0800

    201904271555

--- brad09.php
+++ brad09.php
@@ -2,6 +2,17 @@
 
 $score = rand(0,100);
 echo $score . '<br>';
-if ($score >= 60){
-    echo 'Pass';
+if ($score >= 90){
+    echo 'A';
+}elseif ($score >= 80){
+    echo 'B';
+}elseif ($score >= 70){
+    echo 'C';
+}elseif ($score >= 60){
+    echo 'D';
+}else{
+    echo 'E';
 }
+
+
+
