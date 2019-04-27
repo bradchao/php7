ce36cd9de039b901da2d99039787b18e77b0a00c
user <brad@brad.tw>
2019-04-27 12:00:37 +0800

    201904271200

---
+++ brad01.php
@@ -0,0 +1,17 @@
+OK
+<?php
+/*
+ * This is My First PHP Program
+ *
+ */
+
+
+/**
+ * PHPDoc
+ */
+
+
+eChO 'Hello, Worldv2';  // 大小寫不嚴格區分, 除了變數之外
+?>
+<h1>Brad Big Company</h1>
+<hr />
---
+++ brad03.php
@@ -0,0 +1,16 @@
+<?php
+    $var1 = 1;
+    $var2 = 1.23;
+    $var3 = 'Brad';
+    $var4 = true;
+    echo gettype($var4) . '<br>';
+    $var5 = 12;
+    echo $var5 . '<br>';
+    $var6 = 012;
+    echo $var6 . '<br>';
+    $var7 = 0x12;
+    echo $var7 . '<br>';
+
+
+
+
