2cf27bab7e3322523f8c158685901e96f80d7ea0
user <brad@brad.tw>
2019-04-27 16:37:12 +0800

    201904271637

--- brad11.php
+++ brad11.php
@@ -6,13 +6,13 @@ if (isset($_GET['x'])){
     $result = $x + $y;
 }
 ?>
-<form action="brad04.php" method="get">
+<form action="brad11.php" method="get">
     <input type="text" name="x" value="<?php echo $x; ?>"/>
-    <select>
-        <option>+</option>
-        <option>-</option>
-        <option>x</option>
-        <option>/</option>
+    <select name="op">
+        <option value="1">+</option>
+        <option value="2">-</option>
+        <option value="3">x</option>
+        <option value="4">/</option>
     </select>
     <input type="text" name="y" value="<?php echo $y; ?>" />
     <input type="submit" value="="/>
