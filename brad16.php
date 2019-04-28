2b13c06da64bd2105d13291b483cdd798f01ceeb
user <brad@brad.tw>
2019-04-28 11:01:50 +0800

    201904281101

---
+++ brad16.php
@@ -0,0 +1,14 @@
+<?php
+    $i = 0;
+    for( printBrad(); $i < 10;printLine()){
+        echo "{$i}<br>";
+        $i++;
+    }
+
+    function printBrad(){
+        echo 'Brad<br>';
+    }
+    function printLine(){
+        echo '<hr>';
+    }
+
