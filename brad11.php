de01838bd0a50879a58e3f012372bfdd35380ff1
user <brad@brad.tw>
2019-04-28 09:27:17 +0800

    201904280927

--- brad11.php
+++ brad11.php
@@ -1,5 +1,6 @@
 <?php
 $x = $y = $result = '';
+$op = 1;
 if (isset($_GET['x'])){
     $x = $_GET['x'];
     $y = $_GET['y'];
@@ -23,10 +24,10 @@ if (isset($_GET['x'])){
 <form action="brad11.php" method="get">
     <input type="text" name="x" value="<?php echo $x; ?>"/>
     <select name="op">
-        <option value="1">+</option>
-        <option value="2">-</option>
-        <option value="3">x</option>
-        <option value="4">/</option>
+        <option value="1" <?php if ($op==1) echo 'selected';?>>+</option>
+        <option value="2" <?php if ($op==2) echo 'selected';?>>-</option>
+        <option value="3"  <?php if ($op==3) echo 'selected';?>>x</option>
+        <option value="4" <?php if ($op==4) echo 'selected';?>>/</option>
     </select>
     <input type="text" name="y" value="<?php echo $y; ?>" />
     <input type="submit" value="="/>
