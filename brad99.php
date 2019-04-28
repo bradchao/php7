faa37dc13e9e44bf496cb5c5fc088a6084c0c2e8
user <brad@brad.tw>
2019-04-28 11:45:55 +0800

    201904281145

--- brad99.php
+++ brad99.php
@@ -1,9 +1,13 @@
 <table border="1" width="100%">
     <?php
-        for ($k=0; $k<4; $k++){
+        define("LINES", 2);
+        define("LINE0FROM", 1);
+        define("LINE0TO", 5);
+
+        for ($k=0; $k<LINES; $k++){
             echo '<tr>';
-            for ($j=2; $j<=6; $j++){
-                $newj = $j + $k * 5;
+            for ($j=LINE0FROM; $j<=LINE0TO; $j++){
+                $newj = $j + $k * (LINE0TO-LINE0FROM+1);
                 echo '<td>';
                 for ($i = 1; $i<=9; $i++){
                     $r = $newj * $i;
