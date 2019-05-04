583e11fb6fdc04e4358b018028c3721a8b570515
user <brad@brad.tw>
2019-05-04 09:17:38 +0800

    201905040917

--- pokerv4.php
+++ pokerv4.php
@@ -9,6 +9,37 @@
     foreach ($poker as $i => $card){
         $players[$i % 4][(int)($i/4)] = $card;
     }
-    foreach ($players[1] as $v){
-        echo "{$v}<br>";
-    }
+//    foreach ($players[1] as $v){
+//        echo "{$v}<br>";
+//    }
+//    echo '<hr>';
+    ?>
+
+
+<table border="1">
+    <tr>
+        <?php
+            foreach ($players[0] as $card){
+                echo "<td>{$card}</td>";
+            }
+        ?>
+    </tr>
+    <tr>
+        <td>123</td>
+        <td>123</td>
+        <td>123</td>
+        <td>123</td>
+    </tr>
+    <tr>
+        <td>123</td>
+        <td>123</td>
+        <td>123</td>
+        <td>123</td>
+    </tr>
+    <tr>
+        <td>123</td>
+        <td>123</td>
+        <td>123</td>
+        <td>123</td>
+    </tr>
+</table>
