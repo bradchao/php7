2c25b73d72d4229fd6f4b1df81d5be93793090b9
user <brad@brad.tw>
2019-05-04 14:02:04 +0800

    201905041401

--- bradutil.php
+++ bradutil.php
@@ -1,9 +1,17 @@
 <?php
     function isTWId($id) {
+        $ret = false;
+        if (preg_match("/^[A-Z][12][0-9]{8}$/", $id)){
+            $letters = 'ABCDEFGHJKLMNPQRSTUVXYWZIO';
+            $c1 = substr($id, 0, 1);    // 'A'
+            $n12 = strpos($letters, $c1) + 10;
+            $n1 = (int)($n12 / 10);
+            $n2 = $n12 % 10;
+            $n3 = substr($id, 1,1);
+            $n4 = substr($id, 2,1);
 
-        return true / false;
+        }
+        return $ret;
     }
-
-    echo strlen("abcdefg資策會");
 
 ?>
