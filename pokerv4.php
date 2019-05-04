75c6c4cd37f8f5aeb3b8ac8588cd2a86a381196d
user <brad@brad.tw>
2019-05-04 09:52:34 +0800

    201905040952

--- pokerv4.php
+++ pokerv4.php
@@ -1,45 +1,24 @@
 <?php
     $poker = range(0,51);
     shuffle($poker);
-    foreach ($poker as $v){
-        echo "{$v}<br>";
-    }
-    echo '<hr>';
     $players = array(array(),array(),array(),array());
     foreach ($poker as $i => $card){
         $players[$i % 4][(int)($i/4)] = $card;
     }
-//    foreach ($players[1] as $v){
-//        echo "{$v}<br>";
-//    }
-//    echo '<hr>';
     ?>
-
-
 <table border="1">
-    <tr>
-        <?php
-            foreach ($players[0] as $card){
-                echo "<td>{$card}</td>";
+    <?php
+        $suits = array("&spades;","<font color='red'>&hearts;</font>",
+            "<font color='red'>&diams;</font>","&clubs;");  // 0, 1, 2, 3
+        $values = array('A',2,3,4,5,6,7,8,9,10,'J','Q','K');
+        foreach ($players as $player){
+            sort($player);
+            echo '<tr>';
+            foreach ($player as $card){
+                echo "<td>{$suits[(int)($card/13)]}{$values[$card%13]}</td>";
             }
-        ?>
-    </tr>
-    <tr>
-        <td>123</td>
-        <td>123</td>
-        <td>123</td>
-        <td>123</td>
-    </tr>
-    <tr>
-        <td>123</td>
-        <td>123</td>
-        <td>123</td>
-        <td>123</td>
-    </tr>
-    <tr>
-        <td>123</td>
-        <td>123</td>
-        <td>123</td>
-        <td>123</td>
-    </tr>
+            echo '</tr>';
+        }
+    ?>
 </table>
+
