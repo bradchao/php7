993b42ca26a6fef7c64ec2d629c42ab5e95b347a
user <brad@brad.tw>
2019-05-05 15:23:30 +0800

    201905051523

--- brad49.php
+++ brad49.php
@@ -1,11 +1,15 @@
 <?php
+    $rate = $_GET['rate'];
+
     // 1. new canvas
     $img = imageCreateTrueColor(400, 24);
 
     // 2. drawing
     $yellow = imagecolorallocate($img, 255, 255, 0);
     $green = imagecolorallocate($img, 0, 255, 0);
+    $red = imagecolorallocate($img, 255, 0, 0);
     imagefill($img, 0, 0,$green);
+    imagefilledrectangle($img, 0, 0, 400*$rate/100, 24, $red);
 
     // 3. display
     header("Content-type: image/jpeg");
