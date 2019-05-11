601f748fcedfa93e1fdd088c8df36e4bf75c8311
user <brad@brad.tw>
2019-05-11 16:52:03 +0800

    201905111651

--- brad60.php
+++ brad60.php
@@ -7,9 +7,31 @@
     $stmt->execute();
     $stmt->store_result();
 
+    $ret = new stdClass();
     if ($stmt->num_rows >0 ){
+        $ret->result = 0;
         $stmt->bind_result($id,$cname,$birthday,$memo);
+
+        $row = [];
+        while ($stmt->fetch()){
+            $row['id'] = $id;
+            $row['cname'] = $cname;
+            $row['birthday'] = $birthday;
+            $row['memo'] = $memo;
+
+            $ret->data[] = $row;
+        }
+
+//        foreach ($data as $row){
+//            echo "{$row['id']}:{$row['cname']}:{$row['birthday']}:{$row['memo']}<br>";
+//        }
+
+    }else{
+        $ret->result = -1;
     }
 
+    $stmt->free_result();
+    $stmt->close();
 
+    echo json_encode($ret);
 ?>
