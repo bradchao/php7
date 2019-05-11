269cef1425ddc7e665a52fa7b4fb0c660505fbd6
user <brad@brad.tw>
2019-05-11 16:59:52 +0800

    201905111659

---
+++ addNew.php
@@ -0,0 +1,19 @@
+<form>
+    <table>
+        <tr>
+            <td>Name</td>
+            <td><input></td>
+        </tr>
+        <tr>
+            <td>birthday</td>
+            <td><input></td>
+        </tr>
+        <tr>
+            <td>Memo</td>
+            <td><textarea></textarea></td>
+        </tr>
+        <tr>
+            <td colspan="2"><input type="submit" value="add" /></td>
+        </tr>
+    </table>
+</form>
---
+++ brad61.php
@@ -0,0 +1,22 @@
+<h1>Brad Big Company</h1>
+<hr>
+<a href="addNew.php">New</a>
+<hr/>
+<table border="1" width="100%">
+    <tr>
+        <th>id</th>
+        <th>Name</th>
+        <th>Birthday</th>
+        <th>Tel</th>
+        <th>Delete | Update</th>
+    </tr>
+
+    <tr>
+        <td>3</td>
+        <td>Brad</td>
+        <td>1999-01-02</td>
+        <td>123</td>
+        <td>Delete | Update</td>
+    </tr>
+
+</table>
