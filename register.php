42b16cd88247ffb3db061c1feac1f700b1e5ac48
user <brad@brad.tw>
2019-05-18 09:31:45 +0800

    2019051801

---
+++ register.php
@@ -0,0 +1,7 @@
+<form method="post" enctype="multipart/form-data">
+    Account: <input type="text" name="account" /><br />
+    Password: <input type="password" name="passwd" /><br />
+    Real Name: <input type="text" name="name" /><br />
+    Icon: <input type="file" name="icon" /><br />
+    <input type="submit" value="New" />
+</form>
---
+++ sql.php
@@ -0,0 +1,4 @@
+<?php
+
+    $mysqli = new mysqli('localhost','root','root', 'iii');
+    $mysqli->set_charset('utf-8');
