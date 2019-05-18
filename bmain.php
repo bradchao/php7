35a83f21f150daa6a840bd6fa460ddc7b62a4b2b
user <brad@brad.tw>
2019-05-18 14:41:27 +0800

    2019051809

--- bmain.php
+++ bmain.php
@@ -9,6 +9,16 @@
         $delid = $_REQUEST['delid'];
         $sql = "DELETE FROM product WHERE id = {$delid}";
         $mysqli->query($sql);
+    }else if (isset($_REQUEST['cloneid'])){
+        $cloneid = $_REQUEST['cloneid'];
+        $sql = "INSERT INTO product (pname,price,qty) " .
+                "SELECT pname, price, qty FROM product WHERE id=?";
+        $stmt = $mysqli->prepare($sql);
+        $stmt->bind_param("i", $cloneid);
+        $stmt->execute();
+
+        //echo htmlspecialchars($stmt->error) ;
+
     }
 
 
@@ -45,7 +55,8 @@ Product List:<br />
 
             echo '<td>';
             echo "<a href='?delid={$product->id}' onclick='return confirmDelete(\"{$product->pname}\");'>Del</a>";
-
+            echo " | Update";
+            echo " | <a href='?cloneid={$product->id}'>Copy</a>";
             echo '</td>';
 
             echo '</tr>';
