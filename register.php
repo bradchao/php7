1aa12448a5d70f4138d09b70d0c81f0024bf2f8c
user <brad@brad.tw>
2019-05-18 10:05:34 +0800

    2019051802

--- register.php
+++ register.php
@@ -1,5 +1,25 @@
+<script>
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (xhttp.readyState == 4 && xhttp.status == 200){
+            document.getElementById('mesg').innerHTML = xhttp.responseText;
+        }
+    } 
+
+    function isNewAccount() {
+        var account = document.getElementById('account').value;
+
+        xhttp.open("GET", "isNewAccount.php", true);
+        xhttp.send();
+    }
+
+</script>
+
+
 <form method="post" enctype="multipart/form-data">
-    Account: <input type="text" name="account" /><br />
+    Account: <input type="text" id="account" name="account" onchange="isNewAccount()"/>
+    <span id="mesg"></span>
+    <br />
     Password: <input type="password" name="passwd" /><br />
     Real Name: <input type="text" name="name" /><br />
     Icon: <input type="file" name="icon" /><br />
